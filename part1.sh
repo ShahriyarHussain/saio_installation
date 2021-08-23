2cca3e34727caeedae497fe2d7dda5f30527e832
ShahriyarHussain <shossainshihab36@gmail.com>
2021-08-23 10:02:55 +0600

    Fix startup script not running issue

--- part1.sh
+++ part1.sh
@@ -3,22 +3,23 @@
 sudo echo ""
 sudo touch log_files.log
 sudo chmod 777 log_files.log
+dir=$(pwd)
 
 echo -ne 'Updating Files and installing dependencies [##                     ](5%)\r'
 sleep 1
 {
   sudo apt update
-} &> log_files.log
+} &> $dir/log_files.log
 echo -ne 'Updating Files and installing dependencies [#####                  ](18%)\r'
 sleep 1
 {
   sudo apt install curl gcc memcached rsync sqlite3 xfsprogs git-core libffi-dev python3-setuptools liberasurecode-dev libssl-dev -y
-} &> log_files.log
+} &> $dir/log_files.log
 echo -ne 'Updating Files and installing dependencies [##########             ](67%)\r'
 sleep 1
 {
   sudo apt install python3-coverage python3-dev python3-nose python3-xattr python3-eventlet python3-greenlet python3-pastedeploy python3-netifaces python3-pip python3-dnspython python3-mock -y
-} &> log_files.log
+} &> $dir/log_files.log
 echo -ne 'Updating Files and installing dependencies [#######################](100%)\r'
 echo -ne '\n'
 
@@ -27,23 +28,23 @@ sleep 1
 {
   cd /opt/
   sudo git clone https://github.com/openstack/python-swiftclient.git
-} &> log_files.log
+} &> $dir/log_files.log
 echo -ne 'Cloning Files & Installing requirements [###                    ](5%)\r'
 sleep 1
 {
   cd /opt/python-swiftclient; sudo pip3 install -r requirements.txt; python3 setup.py install; cd-
-} &> log_files.log
+} &> $dir/log_files.log
 echo -ne 'Cloning Files & Installing requirements [#######                ](35%)\r'
 sleep 1
 {
   cd /opt/
   sudo git clone https://github.com/openstack/swift.git
-} &> log_files.log
+} &> $dir/log_files.log
 echo -ne 'Cloning Files & Installing requirements [#########              ](65%)\r'
 sleep 3
 {
 cd /opt/swift; sudo pip3 install -r requirements.txt; sudo python3 setup.py install; cd -
-} &> log_files.log
+} &> $dir/log_files.log
 echo -ne 'Cloning Files & Installing requirements [#######################](100%)\r'
 echo -ne '\n'
 
@@ -60,7 +61,7 @@ sudo cp /opt/swift/etc/object-server.conf-sample /etc/swift/object-server.conf
 sudo cp /opt/swift/etc/proxy-server.conf-sample /etc/swift/proxy-server.conf
 sudo cp /opt/swift/etc/drive-audit.conf-sample /etc/swift/drive-audit.conf
 sudo cp /opt/swift/etc/swift.conf-sample /etc/swift/swift.conf
-} &> log_files.log
+} &> $dir/log_files.log
 echo -ne 'Copying .conf files [#######################](100%)\r'
 echo -ne '\n'
 
@@ -84,14 +85,16 @@ sleep 1
   sudo useradd swift
   sudo chown -R swift:swift /srv/node
 
-  sudo cp /opt/saio_installer/mount_devices.sh /opt/swift/bin/mount_devices.sh
-  sudo cp /opt/saio_installer/start_swift.service /etc/systemd/system/start_swift.service
+  sudo cp $dir/mount_devices.sh /opt/swift/bin/mount_devices.sh
+  sudo cp $dir/mount_drives.service /etc/systemd/system/mount_drives.service
   sudo chmod +x /opt/swift/bin/mount_devices.sh
 
-  sudo systemctl start start_swift.service
-  sudo systemctl enable start_swift.service
-  sudo systemctl restart start_swift.service
-} &> log_files.log
+  sudo systemctl restart mount_drives.service
+  sudo systemctl enable mount_drives.service
+  sudo systemctl start mount_drives.service
+
+
+} &> $dir/log_files.log
 echo -ne 'Mounting Drives and Creating Startup script [#######################](100%)\r'
 echo -ne '\n'
 
@@ -113,5 +116,5 @@ do
   fi
 done
 
-sudo echo "reboot" >> $HOME/saio_installer/temp.txt
+sudo echo "reboot" >> $dir/temp.txt
 sudo reboot
--- part2.sh
+++ part2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bash $HOME/saio_installer/mount_devices.sh
+# bash $dir/mount_devices.sh
 sudo echo ""
 echo "WARNING: ENTER VALUES APPROPRIATELY, THERE ARE NO ERROR CHECKINGS"
 echo "Recommeded values are, part_power=3, replication=3, min_hour=1"
@@ -12,7 +12,6 @@ read min_hour
 
 echo -ne 'Creating Builder files and adding devices [##                     ](5%)\r'
 sleep 1
-
 {
   cd /etc/swift
   sudo swift-ring-builder account.builder create $part_power $replication $min_hour
@@ -21,7 +20,7 @@ sleep 1
   sudo swift-ring-builder object-1.builder create $part_power $replication $min_hour
   sudo swift-ring-builder object-2.builder create $part_power $replication $min_hour
 
-}
+} &> /dev/null
 echo -ne 'Creating Builder files and adding devices [####                   ](10%)\r'
 sleep 1
 {
@@ -38,7 +37,7 @@ sleep 1
   sudo swift-ring-builder object.builder add r1z2-127.0.0.2:6010/d2 100
   sudo swift-ring-builder object-1.builder add r1z2-127.0.0.2:6010/d2 100
   sudo swift-ring-builder object-2.builder add r1z2-127.0.0.2:6010/d2 100
-}
+} &> /dev/null
 echo -ne 'Creating Builder files and adding devices [##########             ](45%)\r'
 sleep 1
 {
@@ -48,7 +47,7 @@ sleep 1
   sudo swift-ring-builder object.builder add r1z3-127.0.0.3:6020/d3 100
   sudo swift-ring-builder object-1.builder add r1z3-127.0.0.3:6020/d3 100
   sudo swift-ring-builder object-2.builder add r1z3-127.0.0.3:6020/d3 100
-} &> log_files.log
+} &> /dev/null
 echo -ne 'Creating Builder files and adding devices [#######################](100%)\r'
 echo -ne '\n'
 
@@ -64,7 +63,7 @@ sleep 1
   sudo swift-ring-builder object.builder rebalance
   sudo swift-ring-builder object-1.builder rebalance
   sudo swift-ring-builder object-2.builder rebalance
-}
+} &> /dev/null
 echo -ne 'Rebalance Builders and Create Logger [######                 ](25%)\r'
 sleep 1
 {
@@ -72,7 +71,7 @@ sleep 1
   sudo bash -c 'echo local1.* /var/log/swift/all1.log > /etc/rsyslog.d/1-swift.conf'
   sudo bash -c 'echo local2.* /var/log/swift/all2.log > /etc/rsyslog.d/2-swift.conf'
 
-  cat /etc/rsyslog.d/0-swift.conf /etc/rsyslog.d/1-swift.conf /etc/rsyslog.d/2-swift.conf
+  # cat /etc/rsyslog.d/0-swift.conf /etc/rsyslog.d/1-swift.conf /etc/rsyslog.d/2-swift.conf
 
   sudo mkdir /var/log/swift
 
@@ -81,12 +80,44 @@ sleep 1
 
   sudo systemctl restart rsyslog
   sudo systemctl enable rsyslog
-} &> log_files.log
+} &> /dev/null
 echo -ne 'Rebalance Builders and Create Logger [#######################](100%)\r'
 echo -ne '\n'
 
-echo "Setup is done. you can start the proxy server and move on. You will most likely need to change the account,container and object ports accordingly"
-sleep 5
+echo "Setup is done. you can start the proxy server"
+sleep 1
+echo "Setup is done. you can start the proxy server"
+sleep 1
+x=0
+for n in {1..50}
+do
+  # echo $n
+  if (( $x == 0 ))
+  then
+    echo -ne 'Finalizing Setup: | \r'
+    sleep 0.2
+    x=1
+  elif (( $x == 1 ))
+  then
+    echo -ne 'Finalizing Setup: / \r'
+    sleep 0.2
+    x=2
+  elif (( $x == 2 ))
+  then
+    echo -ne 'Finalizing Setup: _ \r'
+    sleep 0.2
+    x=3
+  elif (( $x == 3 ))
+  then
+    echo -ne 'Finalizing Setup: \ \r'
+    sleep 0.2
+    x=0
+  else
+    echo -ne 'X \r'
+    sleep 0.2
+  fi
+done
+
 for n in {1..20}
 do
   # echo $n
